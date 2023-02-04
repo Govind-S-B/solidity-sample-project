af7c22646d073b33ea349765088755ff7f799a79
Govind S B <b.s.dnivog@gmail.com>
2023-02-04 11:34:35 +0530

    sending and getting moni

--- test.sol
+++ test.sol
@@ -1,9 +1,18 @@
 pragma solidity 0.8.10;
 
 contract MyContract{
-    mapping(int => int) public map;
+    address public lastSender;
 
-    function setKey(int key,int value) public{
-        map[key]=value;
+    function receive_to_contract() external payable{
+        lastSender = msg.sender;
+    }
+
+    function pay_user(address payable addr,uint eth_amount) public payable{
+        (bool sent,bytes memory data) = addr.call{value: eth_amount*(10**18)}("");
+        require (sent,"Error sending ETH from contract");
+    }
+
+    function getBalance() public view returns (uint){
+        return address(this).balance;
     }
 }
