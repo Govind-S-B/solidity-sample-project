353db8bccaf1dc1c0ec441f4e2a7995269ace94d
Govind S B <b.s.dnivog@gmail.com>
2023-02-06 06:46:52 +0530

    smol bank

--- test.sol
+++ test.sol
@@ -1,18 +1,30 @@
 pragma solidity 0.8.10;
 
-contract MyContract{
-    address public lastSender;
+contract MyBank{
+    mapping(address => uint) private balances;
+    // balances stores balances for each adress in a map
 
-    function receive_to_contract() external payable{
-        lastSender = msg.sender;
+    function deposit() external payable{
+        balances[msg.sender] += msg.value;
+    }
+    // deposit , deposits whatever value was sent in the transaction to the senders adress
+
+    function withdraw(address payable recepient,uint amount) public payable{
+        amount *= 10**18; // wei to eth
+        require(balances[msg.sender]*(10**18) >= amount,"Insufficient Funds");
+        (bool sent,bytes memory data) = recepient.call{value:amount}("");
+        require(sent,"Withdrawal Failed");
+        balances[msg.sender] -= amount;
     }
 
-    function pay_user(address payable addr,uint eth_amount) public payable{
-        (bool sent,bytes memory data) = addr.call{value: eth_amount*(10**18)}("");
-        require (sent,"Error sending ETH from contract");
+    function getBalance() public view returns (uint) {
+        return balances[msg.sender];
     }
 
-    function getBalance() public view returns (uint){
+    function getContractBalance() public view returns (uint) {
         return address(this).balance;
     }
+    
+    // withdraw , withdraws money to the specified address given from the balance of the
+    // user making the request
 }
